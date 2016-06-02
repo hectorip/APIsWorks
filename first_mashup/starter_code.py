f0e0a10616e51308a9f8a27f8ccf9e8a74967e10
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2016-06-01 23:58:21 -0500

    Starter code template

---
+++ first_mashup/starter_code.py
@@ -0,0 +1,19 @@
+from geocode import getGeocodeLocation
+import json
+import httplib2
+
+import ys
+import codecs
+
+sys.stdout = codecs.getwriter('utf8')(sys.stdout)
+sys.stderr = codecs.getwriter('utf8')(sys.stderr)
+
+forsquare_client_id = ""
+forsquare_client_secret = ""
+
+def findARestaurant(mealType, location):
+    pass
+
+if __name__ -- '__main__':
+    findARestaurant("Pizza", "Peña de Jaltemba")
+    findARestaurant("Fish", "Los Ayala")
